93cb50b786e312b9e9cdfda6bc5cb454a224d8c8
Noga lavan <noga0802@gmail.com>
2021-11-01 10:54:49 +0200

    feat(map): add map to screen

--- client/src/components/MainPage/Map/index.js
+++ client/src/components/MainPage/Map/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { styled } from '@mui/system';
 import { Box } from '@mui/material';
+import { MapContainer, TileLayer } from "react-leaflet";
 
-const StyledMap = styled(Box)({
-	background: '#D9FFF5',
-	height: '100%',
-	width: '100%',
-});
+// const StyledMap = styled(Box)({
+// 	height: '100%',
+// 	width: '100%',
+// });
 
-export default () => <StyledMap />;
+const defaultPosition = [31.9679, 34.8325]; 
+
+export default () => {
+
+	return (
+		// <StyledMap>
+		  <MapContainer
+			center={defaultPosition}
+			zoom={13}
+			style={{ height: '100vh', width: '100wh' }}
+		  >
+			<TileLayer
+			  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+		  </MapContainer>
+		// </StyledMap>
+	  );
+};
