c2ad886383ca9923cb807320ca91e7ecb70bf0de
benben12341 <benpro214@gmail.com>
2021-11-02 09:35:19 +0200

    feat(marks-on-maps):add marks of sites to the map

--- client/src/components/MainPage/Map/index.js
+++ client/src/components/MainPage/Map/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+// eslint-disable-next-line object-curly-newline
+import { MapContainer, Marker, TileLayer, Tooltip } from 'react-leaflet';
+import { places } from './places';
 
 const defaultPosition = [31.9679, 34.8325];
 
 export default () => (
   <MapContainer center={defaultPosition} zoom={13} style={{ height: '100%', width: '100%' }}>
+    {places.map(place => (
+      <Marker key={place.id} position={[place.longitude, place.latitude]}>
+        <Tooltip>{place.name}</Tooltip>
+      </Marker>
+    ))}
     <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
---
+++ client/src/components/MainPage/Map/places.js
@@ -0,0 +1,32 @@
+export const places = [
+  {
+    id: 1,
+    name: 'collage of management',
+    adress: 'Rishon le zion',
+    description: 'מקום חרא',
+    longitude: 31.9698651,
+    latitude: 34.7708692,
+    contactName: 'ben',
+    contactNumber: '054-8705464'
+  },
+  {
+    id: 2,
+    name: 'Makmar',
+    adress: 'Rishon le zion',
+    description: 'מקום עם פוטנציאל ',
+    longitude: 31.9679,
+    latitude: 34.8325,
+    contactName: 'ben',
+    contactNumber: '054-8705464'
+  },
+  {
+    id: 3,
+    name: 'נופי פרת',
+    adress: 'Jerusalem',
+    description: 'מקום בלי פוטנציאל ',
+    longitude: 31.825413417692864,
+    latitude: 35.32047596154651,
+    contactName: 'noga',
+    contactNumber: '054-8705464'
+  }
+];
